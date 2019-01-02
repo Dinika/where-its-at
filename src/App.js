583544525cb617a12fa86c384b73790cf6e87e16
Dinika <dinikasaxenas@gmail.com>
2019-01-02 14:48:01 +0530

    global: use semantic-react-ui as ui framework
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { Header, Button } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Where it's at?
+        <Header as='h1'>Where it's at?</Header>
       </div>
     );
   }
