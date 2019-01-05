00a277ac58dc48c44b60d73bf335d19aa476fa5a
Dinika <dinikasaxenas@gmail.com>
2019-01-05 20:32:41 +0530

    use grid to arrange items on homepage
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

---
+++ src/components/ResultTable/ResultTable.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import { Icon, Menu, Table } from 'semantic-ui-react'
+
+class ResultTable extends Component {
+    render() {
+        return (
+            <Table celled>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Social Account</Table.HeaderCell>
+                        <Table.HeaderCell>File</Table.HeaderCell>
+                        <Table.HeaderCell>Time</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    <Table.Row>
+                        <Table.Cell>Cell</Table.Cell>
+                        <Table.Cell>Cell</Table.Cell>
+                        <Table.Cell>Cell</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>Cell</Table.Cell>
+                        <Table.Cell>Cell</Table.Cell>
+                        <Table.Cell>Cell</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                        <Table.Cell>Cell</Table.Cell>
+                        <Table.Cell>Cell</Table.Cell>
+                        <Table.Cell>Cell</Table.Cell>
+                    </Table.Row>
+                </Table.Body>
+
+                <Table.Footer>
+                    <Table.Row>
+                        <Table.HeaderCell colSpan='3'>
+                            <Menu pagination>
+                                <Menu.Item as='a' icon>
+                                    <Icon name='chevron left' />
+                                </Menu.Item>
+                                <Menu.Item as='a'>1</Menu.Item>
+                                <Menu.Item as='a'>2</Menu.Item>
+                                <Menu.Item as='a'>3</Menu.Item>
+                                <Menu.Item as='a'>4</Menu.Item>
+                                <Menu.Item as='a' icon>
+                                    <Icon name='chevron right' />
+                                </Menu.Item>
+                            </Menu>
+                        </Table.HeaderCell>
+                    </Table.Row>
+                </Table.Footer>
+            </Table>
+        )
+    }
+}
+
+export default ResultTable;
--- src/containers/Layout/Layout.jsx
+++ src/containers/Layout/Layout.jsx
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react';
 import Aux from '../../HOC/Aux/Aux';
 import TopMenu from '../../components/TopMenu/TopMenu';
 import SearchBox from '../../components/SearchBox/SearchBox';
 import SocialAccountCards from '../../components/SocialAccountCards/SocialAccountCards';
+import ResultTable from '../../components/ResultTable/ResultTable';
 
 class Layout extends Component {
     render() {
         return (
             <Aux>
                 <TopMenu />
-                <SearchBox />
-                <SocialAccountCards />
-                <div>table</div>
+                    <Grid container>
+                        <Grid.Row>
+                            <Grid.Column textAlign='center'>
+                                <SocialAccountCards />
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Grid.Column textAlign='center' stretched>
+                                <SearchBox />
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Grid.Column>
+                                <ResultTable />
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
             </Aux>
         )
     }
